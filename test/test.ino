7ccea1ecfb55c562af1afe59653492d42e9a9c50
yetongxue <yexy@knownsec.com>
2021-08-21 23:57:11 +0800

    ok

--- test/test.ino
+++ test/test.ino
@@ -1,18 +1,45 @@
-int lastHiHatLevel;
-
+#define HI_HAT_LEVEL_CHCHE_LEN 3
+short hiHatLevelCache[HI_HAT_LEVEL_CHCHE_LEN];
+short currenthiHatLevelIndex;
+short lastHiHatLevel;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(13, INPUT);
-  lastHiHatLevel = 1;
+  pinMode(12, INPUT);
+  lastHiHatLevel = 0;
+  currenthiHatLevelIndex = 0;
 }
 
 void loop() {
     int currentLevel = digitalRead(13);
-    if (currentLevel == 1) {
-      Serial.println(currentLevel);
-    }  
+    int l12 = digitalRead(12);
+    hiHatLevelCache[currenthiHatLevelIndex] = currentLevel;
+    Serial.println(currentLevel);
 
+    int sum = 0;
+    for(short i=0; i<HI_HAT_LEVEL_CHCHE_LEN; ++i)
+    {
+      sum += hiHatLevelCache[i];
+    }
+    if (sum == HI_HAT_LEVEL_CHCHE_LEN) {
+//      Serial.println(1);
+//        if (lastHiHatLevel == 0) Serial.println(1);
+        lastHiHatLevel = 1;
+    }
+    if (sum == 0) {
+//       Serial.println(0);
+      if (lastHiHatLevel == 1) {
+//        Serial.println(0);
+        lastHiHatLevel = 0;
+
+        }
+    }
+        
+    currenthiHatLevelIndex += 1;
+    if (currenthiHatLevelIndex >= HI_HAT_LEVEL_CHCHE_LEN) {
+      currenthiHatLevelIndex = 0;
+    }
   
 //  if ((currentLevel == 0) && (lastHiHatLevel == 1 )) {
 //    Serial.println("test");
